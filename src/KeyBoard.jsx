57a849f34e31916a50c581e998210e0aa7205068
BirdNoFly161 <sharp.oussama@gmail.com>
2023-06-10 18:40:37 +0100

    added caps lock to change view and changed grid layout management to use flexbox

--- src/KeyBoard.jsx
+++ src/KeyBoard.jsx
@@ -2,14 +2,26 @@ import React, {useState, useEffect} from "react";
 import KeyBoardRow from './KeyBoardRow.jsx'
 
 const KeyBoard = () => {
+    
     const [rows, setRows] = useState(window.keyboard_layouts.ISO["alpha_numerics"]) 
+    const [rows_alt, setRowsAlt] = useState(window.keyboard_layouts.ISO["alpha_numerics_alt"])
     const [loading, setLoading] = useState(true)
+    
+    // TODO: instead of assuming initial false, check if the system has it enabled or not, most likely using a library
+    // ISSUE: the characters key event generate dont take into account wether caps lock is on
+    // maybe store both normal and alt values in the same component
+    const [capsLock, setCapsLock] = useState(false)
+    const [shiftIsDown, setShiftIsDown] = useState(false)
+    
     useEffect(()=>{
 
     // i mean is there truly no better way to do this ? ...
-    //window.addEventListener('keydown', handleKeyPress)
+    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener('keyup', handleKeyUp)
 
 
+
+    // set main alpha numeric keys
     setRows( rows.map( (row, index)=>
         window.keyboard_layouts.ISO["special_keys_first"][index]
         .concat(row.split(""))
@@ -18,25 +30,34 @@ const KeyBoard = () => {
     )
     setRows(rows => rows.concat([window.keyboard_layouts.ISO["last_row"]]))
     
-    setLoading(false)
-    console.log('breakpoint3', rows)
-    
-    //console.log("break point 1")
-    //console.log(rows)
-    /*console.log(rows.map( (row, index)=>
+    // set alternate alpha numeric keys, triggered by caps lock or shift
+    setRowsAlt( rows_alt.map( (row, index)=>
     window.keyboard_layouts.ISO["special_keys_first"][index]
     .concat(row.split(""))
     .concat(window.keyboard_layouts.ISO["special_keys_last"][index])
-))
-*/
+)
+)
+    setRowsAlt(rows_alt => rows_alt.concat([window.keyboard_layouts.ISO["last_row"]]))
+
+    setLoading(false)
+    console.log('breakpoint3', rows)
     
 }, []);
 
-const handleKeyPress= (e) => {
-    console.log('event recognized', e);
+const handleKeyDown= (e) => {
+    if(e.code === 'CapsLock'){
+        setCapsLock(capsLock => !capsLock)
+    }
 
-    if(e.key === 'd'){
-        console.log('D key press event recognized')
+    if(e.code ==='ShiftLeft'){
+        setShiftIsDown(shiftIsDown => true)
+    }
+}
+
+const handleKeyUp= (e) => {
+
+    if(e.code ==='ShiftLeft'){
+        setShiftIsDown(shiftIsDown => false)
     }
 }
 
@@ -46,11 +67,20 @@ const handleKeyPress= (e) => {
     }
     else{
         return (
-            <div className="keyboard" onKeyDown={handleKeyPress} tabIndex={-1}>
-                {rows.map( (row, index) => 
-                    <KeyBoardRow row={row}/>
-                )}
-            </div>
+
+            shiftIsDown
+            ? <div className="keyboard" onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={-1}>
+                {
+                    rows_alt.map( (row, index) => <KeyBoardRow row={row}/>)
+                }
+               </div>
+            
+            : <div className="keyboard" onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={-1}>
+            {
+                rows.map( (row, index) => <KeyBoardRow row={row}/>)
+            }
+           </div>
+
         );
     }
 
--- src/key.jsx
+++ src/key.jsx
@@ -3,6 +3,9 @@ import React, {useState, useEffect} from "react";
 const Key = (props) => {
 
     const [isDown, setIsDown] = useState(false)
+    const special_keys = window.keyboard_layouts.ISO["special_keys_first"]
+                   .concat(window.keyboard_layouts.ISO["special_keys_last"])
+                   .concat(window.keyboard_layouts.ISO["last_row"])
     
     useEffect(()=>{
         window.addEventListener('keydown', handleKeyDown)
@@ -12,7 +15,7 @@ const Key = (props) => {
 
 
     const handleKeyDown= (event)=>{
-        if(event.key === props.keychar || event.keyCode === props.keychar){
+        if(event.key === props.keychar || event.code === props.keychar){
             console.log('event down localy recognized with key = ', props.keychar)
             if(! isDown){
                 setIsDown(isDown => true)
@@ -21,16 +24,18 @@ const Key = (props) => {
     }
 
     const handleKeyUp= (event)=>{
-        if(event.key === props.keychar || event.keyCode === props.keychar){
+        if(event.key === props.keychar || event.code === props.keychar){
             console.log('event up localy recognized with key = ', props.keychar)
+            setIsDown(isDown => false)
         }
 
         
-        setIsDown(isDown => false)
     }
 
     return (
-        <div className={"key"+ (isDown? " active" : "")}>
+
+        //TODO include the keycodes check as well when trying to add the special-key class
+        <div className={"key"+ (isDown? " active" : "") + (special_keys.includes(props.keychar)? " special-key-"+props.keychar : "")}>
             {props.keychar}
         </div>
     );
