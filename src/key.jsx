923ecd2ca51d2d1a6d14bcbab1a714bb4b018476
chadjaa-sofiane <sofianne012@gmail.com>
2023-06-14 20:45:25 +0100

    displaying the component for testing.

--- src/key.jsx
+++ src/key.jsx
@@ -1,44 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import SpaceBar from "../assets/space_bar.svg";
 
 const Key = (props) => {
+  const [isDown, setIsDown] = useState(false);
+  const special_keys = window.keyboard_layouts.ISO["special_keys_first"]
+    .concat(window.keyboard_layouts.ISO["special_keys_last"])
+    .concat(window.keyboard_layouts.ISO["last_row"]);
 
-    const [isDown, setIsDown] = useState(false)
-    const special_keys = window.keyboard_layouts.ISO["special_keys_first"]
-                   .concat(window.keyboard_layouts.ISO["special_keys_last"])
-                   .concat(window.keyboard_layouts.ISO["last_row"])
-    
-    useEffect(()=>{
-        window.addEventListener('keydown', handleKeyDown)
-        window.addEventListener('keyup', handleKeyUp)
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+  }, []);
 
-    },[])
-
-
-    const handleKeyDown= (event)=>{
-        if(event.key === props.keychar || event.code === props.keychar){
-            console.log('event down localy recognized with key = ', props.keychar)
-            if(! isDown){
-                setIsDown(isDown => true)
-            }
-        }
+  const handleKeyDown = (event) => {
+    if (event.key === props.keychar || event.code === props.keychar) {
+      console.log("event down localy recognized with key = ", props.keychar);
+      if (!isDown) {
+        setIsDown((isDown) => true);
+      }
     }
+  };
 
-    const handleKeyUp= (event)=>{
-        if(event.key === props.keychar || event.code === props.keychar){
-            console.log('event up localy recognized with key = ', props.keychar)
-            setIsDown(isDown => false)
-        }
-
-        
+  const handleKeyUp = (event) => {
+    if (event.key === props.keychar || event.code === props.keychar) {
+      console.log("event up localy recognized with key = ", props.keychar);
+      setIsDown((isDown) => false);
     }
+  };
 
-    return (
+  return (
+    /* //TODO include the keycodes check as well when trying to add the
+      special-key class
+      */
+    <div
+      className={
+        "key" +
+        (isDown ? " active" : "") +
+        (special_keys.includes(props.keychar)
+          ? " special-key-" + props.keychar
+          : "")
+      }
+    >
+      {props.keychar}
+    </div>
+  );
+};
 
-        //TODO include the keycodes check as well when trying to add the special-key class
-        <div className={"key"+ (isDown? " active" : "") + (special_keys.includes(props.keychar)? " special-key-"+props.keychar : "")}>
-            {props.keychar}
-        </div>
-    );
-}
-
-export default Key
+export default Key;
